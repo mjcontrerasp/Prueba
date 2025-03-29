5540ace3ee364b1c9789b7e205d9420025acebe1
mjcontrerasp <mjcontrerasp01@educarex.es>
2025-03-29 14:14:04 +0100

    Add files via upload

---
+++ Pruebas/src/App.java
@@ -0,0 +1,45 @@
+
+import java.util.Scanner;
+
+public class App {
+    public static final Scanner sc = new Scanner(System.in);
+
+    public static int dividir(int a, int b) {
+        return a / b;
+    }
+
+    public static void metodoLargo() {
+        System.out.println("Línea 1");
+        System.out.println("Línea 1");
+        System.out.println("Línea 1");
+        System.out.println("Línea 1");
+        System.out.println("Línea 1");
+        System.out.println("Línea 1");
+        System.out.println("Línea 1");
+        System.out.println("Línea 1");
+        System.out.println("Línea 1");
+        System.out.println("Línea 1");
+        System.out.println("Línea 1");
+        System.out.println("Línea 1");
+        System.out.println("Línea 1");
+        System.out.println("Línea 1");
+        System.out.println("Línea 1");
+        System.out.println("Línea 1");
+        System.out.println("Línea 1");
+        System.out.println("Línea 1");
+        System.out.println("Línea 1");
+        System.out.println("Línea 1");
+        System.out.println("Línea 1");
+        System.out.println("Línea 1");
+        
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println("Dame los números a dividir:");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        dividir(num1, num2);
+        System.out.println();
+        metodoLargo();
+        
+    }
+}
